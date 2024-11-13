558773633231e21f7d8f528aa5f4c769769b18dc
Weslley Thiago <weslleyothiago@gmail.com>
2024-11-13 02:25:59 -0300

    implement login generating JWT

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,9 +4,11 @@ import {
   HttpStatus,
   Post,
   UseGuards,
+  Request,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthRequest } from './models/AuthRequest';
 
 @Controller('')
 export class AuthController {
@@ -15,7 +17,7 @@ export class AuthController {
   @Post('login')
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
-  login() {
-    // return this.authService.login();
+  login(@Request() req: AuthRequest) {
+    return this.authService.login(req.user);
   }
 }
