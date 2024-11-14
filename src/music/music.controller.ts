051315c8d1073cd142f0aef522bc97a8b8271b5a
Weslley <weslleyothiago@gmail.com>
2024-11-14 04:29:23 -0300

    tudo

---
+++ src/music/music.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { MusicService } from './music.service';
+import { CreateMusicDto } from './dto/create-music.dto';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+
+@Controller('musics')
+export class MusicController {
+  constructor(
+    private readonly musicService: MusicService
+  ) {}
+
+  @IsPublic()
+  @Post('')
+  async create(@Body() createMusicDto: CreateMusicDto) {
+    return this.musicService.create(createMusicDto);
+  }
+
+  @IsPublic()
+  @Get()
+  async findAll() {
+    return this.musicService.findAll();
+  }
+
+  @IsPublic()
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.musicService.findOne(id);
+  }
+}
---
+++ src/music/music.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateMusicDto } from './dto/create-music.dto';
+
+@Injectable()
+export class MusicService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateMusicDto) {
+    return this.prisma.musica.create({ 
+      data: {
+        titulo: data.title, // Mapeando title para titulo
+        duracao: data.duration, // Mapeando duration para duracao
+        link: data.link,
+        slug: data.slug,
+        fkGeneroMusicalId: data.genreId, // Mapeando genreId para fkGeneroMusicalId
+      }
+     });
+  }
+
+  async findAll() {
+    return this.prisma.musica.findMany();
+  }
+
+  async findOne (id: string) {
+    return this.prisma.musica.findUnique({
+      where: {
+        id: Number(id)
+      }
+    })
+  }
+}
