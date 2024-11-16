8dff961d5cc6cf0d6bf9c153c5f64dd67ad5dce4
Weslley <weslleyothiago@gmail.com>
2024-11-16 00:13:39 -0300

    registering user and profile at same time

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Controller('user')
 export class UserController {
@@ -9,7 +10,10 @@ export class UserController {
 
   @IsPublic()
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  async create(@Body() body: { createUserDto: CreateUserDto, createProfileDto: CreateProfileDto }) {
+    console.log('Received body:', body);  // Adicione um log para verificar os dados recebidos
+    const { createUserDto, createProfileDto } = body;
+    return this.userService.create(createUserDto, createProfileDto);
   }
+  
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,27 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { CreateProfileDto } from './dto/create-profile.dto';
+import { CreateProfileDto } from './dto/create-profile.dto'; // DTO do perfil
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(
-    createUserDto: CreateUserDto,
-    createProfileDto: CreateProfileDto
-  ) {
+  async create(createUserDto: CreateUserDto, createProfileDto: CreateProfileDto) {
+    // Hash da senha do usuário
     const data = {
       ...createUserDto,
       senha: await bcrypt.hash(createUserDto.senha, 10),
     };
 
-    const createdUser = await this.prisma.usuario.create({ data });
+    // Inicia uma transação para criar o usuário e o perfil simultaneamente
+    const createdUser = await this.prisma.$transaction(async (prisma) => {
+      // Criação do usuário
+      const user = await prisma.usuario.create({ data });
 
+      // Criação do perfil associado ao usuário recém-criado
+      const profile = await prisma.perfil.create({
+        data: {
+          nome: createProfileDto.nome,
+          fotoPerfil: createProfileDto.fotoPerfil,
+          dataNascimento: new Date(createProfileDto.dataNascimento),
+          slug: createProfileDto.slug,
+          fkUsuarioId: user.id, // Associando o perfil ao usuário
+        },
+      });
+
+      return { user, profile };  // Retorna o usuário e o perfil criados
+    });
+
+    // Retorna o usuário e o perfil criado sem a senha
     return {
-      ...createdUser,
-      senha: undefined,
+      user: {
+        ...createdUser.user,
+        senha: undefined,  // Não retorna a senha do usuário
+      },
+      profile: createdUser.profile, // Retorna o perfil criado
     };
   }
 
