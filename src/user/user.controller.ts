8288087b522a45731fd31b02f65a5f43b46dd4cd
Weslley <weslleyothiago@gmail.com>
2024-11-24 15:00:14 -0300

    Backup (Vou tornar a relação de perfil e usuários um-para-um)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Req } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
@@ -6,12 +6,13 @@ import { CreateProfileDto } from './dto/create-profile.dto';
 
 @Controller('user')
 export class UserController {
+  prisma: any;
   constructor(private readonly userService: UserService) {}
 
   @IsPublic()
   @Post()
   async create(@Body() body: { createUserDto: CreateUserDto, createProfileDto: CreateProfileDto }) {
-    console.log('Received body:', body);  // Adicione um log para verificar os dados recebidos
+    console.log('Received body:', body); 
     const { createUserDto, createProfileDto } = body;
     return this.userService.create(createUserDto, createProfileDto);
   }
@@ -28,5 +29,18 @@ export class UserController {
     const nameExists = await this.userService.checkNameExisting(name);
     return { nameExists };
   }  
+
+  @Get('profiles')
+  async getUserPerfis(@Req() req) {
+    const userId = req.user.sub;
+
+    const perfis = await this.prisma.perfil.findMany({
+      where: {
+        fkUsuarioId: userId,
+      },
+    });
+
+    return perfis;
+  }
 
 }
